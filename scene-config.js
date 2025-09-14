e12f256e7e7a1c08a1c28aed8c45808cf671701e
soslan-yunona16 <geminiuser20252025@gmail.com>
2025-09-14 23:37:17 +0400

    Update scene-config.js

--- scene-config.js
+++ scene-config.js
@@ -191,7 +191,7 @@ const sceneConfig = {
     "borderThickness": 0.015
   },
   "intro_animation": {
-    "enabled": true,
+    "enabled": false,
     "duration": 2.5,
     "photo_effect": {
       "enabled": true,
